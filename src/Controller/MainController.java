124b678dc4e31e1d2c9fb0f0aff24578cf6eaa2c
illihcs <j.schillemans@gmx.de>
2015-05-26 22:39:22 +0200

    - added first JUnit-Test for ITermin - tests: create, edit, getAll --> not delete, because of deleting real data

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,25 +1,25 @@
 package Controller;
 
+import JUnit.TerminTest;
 import Model.SqlConfig;
 import View.UserViews.Login;
 
 import javax.swing.*;
 
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.JUnit4;
+import org.junit.runners.model.InitializationError;
+
 /**
  * Created by Schillemans on 01.05.2015.
  */
 public class MainController {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InitializationError
     {
         Login loginApplicationUser = new Login();
         loginApplicationUser.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         loginApplicationUser.setSize(500, 200);
         loginApplicationUser.setVisible(true);
         loginApplicationUser.setLocationRelativeTo(null);
-
-
-
-
-
     }
 }
